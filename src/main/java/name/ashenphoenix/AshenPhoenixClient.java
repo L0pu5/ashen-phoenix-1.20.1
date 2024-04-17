fb1980fcae5b4e7f54f8e7a7e3394244f26e8e80
L0pu5 <lucasjblackwell2007@gmail.com>
2024-04-16 18:45:16 -0600

    commit

---
+++ src/main/java/name/ashenphoenix/AshenPhoenixClient.java
@@ -0,0 +1,10 @@
+package name.ashenphoenix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AshenPhoenixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
